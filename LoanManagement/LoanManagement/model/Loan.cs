17578d0a7f49028d9e2daee19f38777186d146a5
rik73 <117023539+rik73@users.noreply.github.com>
2024-10-05 13:05:52 +0530

    Add files via upload

---
+++ LoanManagement/LoanManagement/model/Loan.cs
@@ -0,0 +1,26 @@
+﻿namespace LoanManagement.model
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        public Customer Customer { get; set; }
+        public decimal PrincipalAmount { get; set; }
+        public decimal InterestRate { get; set; }
+        public int LoanTerm { get; set; }
+        public string LoanType { get; set; }
+        public string LoanStatus { get; set; }
+
+        public Loan(int loanId, Customer customer, decimal principalAmount, decimal interestRate, int loanTerm, string loanType, string loanStatus)
+        {
+            LoanId = loanId;
+            Customer = customer;
+            PrincipalAmount = principalAmount;
+            InterestRate = interestRate;
+            LoanTerm = loanTerm;
+            LoanType = loanType;
+            LoanStatus = loanStatus;
+        }
+
+        public Loan() { }
+    }
+}
---
+++ LoanManagement/LoanManagement/util/DBConnectUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace LoanManagement.util
+{
+    public static class DBConnectUtil
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = GetConnectionString();
+            return new SqlConnection(connectionString);
+        }
+
+        private static string GetConnectionString()
+        {
+
+            string path = "appsettings.json";
+            if (File.Exists(path))
+            {
+                var json = File.ReadAllText(path);
+                var config = Newtonsoft.Json.JsonConvert.DeserializeObject<ConnectionConfig>(json);
+                return config.ConnectionString;
+            }
+            else
+            {
+                throw new FileNotFoundException("Configuration file not found.");
+            }
+        }
+
+        private class ConnectionConfig
+        {
+            public string ConnectionString { get; set; }
+        }
+    }
+}
